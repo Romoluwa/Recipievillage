1a6dd711459a4050911ef09067ec424cfdfe8f60
quaddss52 <tomilayojesse@gmail.com>
2025-05-18 18:54:41 +0100

    fixed

--- src/pages/recipies.jsx
+++ src/pages/recipies.jsx
@@ -4,13 +4,9 @@ export default function Recipies() {
   return (
     <div className="recipiePage">
       <Navbar />
-
-      <div className="searchBar">
-        <input
-          type="text"
-          placeholder="Search for a recipie!"
-          className="input"
-        />
+      <div className="recipeContainer">
+        <input type="text" className="searchInput" />
+        {/* your remaining component should fall here */}
       </div>
     </div>
   );
